3906dd310185674e20b6fafc76abf25d1884a16a
jihench09 <jhen.chaouachi@outlook.com>
2026-01-05 11:57:45 +0100

    fezrdf

--- Config/Mailer.js
+++ Config/Mailer.js
@@ -1,17 +1,15 @@
 const nodemailer = require("nodemailer");
 require("dotenv").config();
 
+
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 587,
-  secure: false, // STARTTLS
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS, // <-- App Password Gmail (16 caractères)
+    pass: process.env.EMAIL_PASS,
   },
   connectionTimeout: 15000,
-  greetingTimeout: 15000,
-  socketTimeout: 15000,
 });
-
 module.exports = transporter;
